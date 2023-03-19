132c83bf7744f09333125242735e804d6286a585
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-03-19 21:35:14 +0200

    Weather Service working

--- CombineWeatherApp/Utils/Constants.swift
+++ CombineWeatherApp/Utils/Constants.swift
@@ -1,8 +1,8 @@
 import Foundation
 
-struct Constants{
+struct Constants {
     
     struct URLs{
-        static let weather = "ttps://api.openweathermap.org/data/2.5/weather?q=Durban&appid=eae28478e34c91fb1aa982fd794df659&units=metric"
+        static let weather = "Https://api.openweathermap.org/data/2.5/weather?q=Durban&appid=eae28478e34c91fb1aa982fd794df659&units=metric"
     }
 }
--- CombineWeatherApp/ViewController.swift
+++ CombineWeatherApp/ViewController.swift
@@ -1,10 +1,27 @@
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weatherTempLabel: UILabel!
+    
+    /// We want to retain the instance
+    private var webservice: Webservice = Webservice()
+    private var cancellable: AnyCancellable? /// when we don't know about the publisher being used
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.cancellable = self.webservice.fetchWeather(city: "AHOY")
+            .catch{_ in Just(Weather.placeholder)}
+            .map{ weather in
+                if let temp = weather.temp{
+                    return "\(temp) C"
+                } else{
+                    return "Error getting weather"
+                }
+            }
+            .assign(to: \.text, on: self.weatherTempLabel)
     }
 
 
